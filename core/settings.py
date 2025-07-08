8d3bef7d1124f62703bcc33306d3019c0c40c32c
kennedyngigi4 <kennedyngigi4@gmail.com>
2025-07-08 05:14:47 +0300

    default db settings fix

--- core/settings.py
+++ core/settings.py
@@ -117,14 +117,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_ACCOUNTS"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    },
+    'default': {},
     'accounts': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_ACCOUNTS"),
